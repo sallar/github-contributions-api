f1936210714929e299d5610687c36db676e579bd
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-05-04 21:07:35 +0300

    Add tokens

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_CONSUMER_KEY,
-  access_token_secret: process.env.TWITTER_CONSUMER_KEY
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 const COLOR_MAP = {
